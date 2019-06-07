0f931d7dc7f4adcbb7b390bc24f4cebb0ae59c6f
Dreaking <w1226230972@163.com>
2019-06-07 21:58:08 +0800

    更新了图片选择器

--- lib/pages/shoucang/index.dart
+++ lib/pages/shoucang/index.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:my_flutter/common/tinker.dart';
 
 class ShoucangIndex extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Center(
-      child: Text("收藏"),
+    return new Container(
+      child: new Container(
+        child: Tinker.Select_Image_picker(
+            Image_height: 120,
+            Image_width: 120,
+            count: 5,
+            line_count: 3,
+            spacing: 13,
+            runSpacing: 10,
+            Click_Image_file: new Image.asset(
+              "image/tianjiatupian.png",
+              width: 120,
+              height: 120,
+              fit: BoxFit.fill,
+            ),
+            callback: (da) {
+              print(da);
+            }),
+      ),
     );
   }
 }
